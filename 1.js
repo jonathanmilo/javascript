f411f341ee25842aba575fc9baee2f59829661cd
jonathan <jonathan.mayan@bue.edu.ar>
2021-03-05 23:10:27 -0300

    5marzo

--- 1.js
+++ 1.js
@@ -1,19 +1,58 @@
-let nombre= prompt("escriba su nombre"),
-    edad= prompt ("escriba su edad");
+// deuda neta Macri 82.400 .000.000 usd
+// deuda al final del mandato 323.065.000.000 usd
+
+//deuda neta Cristina Kirchner 75.554.000.000 usd
+//deuda al final del mandato 240.665.000.000 usd
+// fuente Ministerio de finanzas,consultado 3/3/2021
+let opcionPresidencias= document.getElementById("opcionPresidencias").value;
+
+if (opcionPresidencias =="Mauricio Macri"){ console.log("presidente Mauricio")
+    deudaNeta= Number (82400000000),deudaTotal=Number (323065000000)}
+else if (opcionPresidencias =="Cristina Fernandez de Kirchner"){
+    deudaNeta= Number(75554000000),deudaTotal= Number(240665000000),console.log("presidente Cristina")
+}else{ console.log('sin designación de presidencia')}
+
+   
+let valorPeso= Number(document.getElementById("valor_delPeso").value);
+let tipo_billetePesos= Number(document.getElementById("input_denominacionBillete").value);
+let enPesos= valorPeso / tipo_billetePesos ;
+
+info_deudaNeta= deudaNeta;
+info_deudaTotal= deudaTotal
+
+superficie_horizontalNeta_Pesos= deudaNeta * enPesos * 0.10075+" m2";
+superficie_horizontalTotal_Pesos= deudaTotal * enPesos * 0.10075 + "m2";
+
+superficie_verticalNeta_Pesos= deudaNeta * enPesos * 0.002 + " metros de altura";
+superficie_verticalTotal_pesos= deudaTotal * enPesos * 0.002 + " metros de altura";
+
+superficie_hotizontalNeta_1usd= deudaNeta * 0.10338947064 +" m2";
+superficie_horizontalTotal_1usd= deudaTotal * 0.10338947064 + " m2";
+
+superficie_verticalNeta_1usd= deudaNeta * 0.002 + " metros de altura";
+superficie_verticalTotal_1usd= deudaTotal * 0.002 + " metros de altura";
+
+let tipoMoneda= (document.getElementById("input_tipoMoneda"));
+
+let mensaje= document.getElementById("campo_Informe") 
+
+
+
+function infoDatos() {
     
-    if(edad <28){console.log("usted es menor de 28 años, el contenido se mostrará parcialmente")}
-    else if(nombre !="jonathan")
-        {console.log("bienvenido"+" "+nombre+ ", este usuario cuenta con acceso restingido")}
-    else if(nombre== "usuario1"||"jonathan"){ console.log("acceso total")}
-    else if(nombre== "jonathan"&& edad ==28)
-    { var titulo = document.getElementById('titutlo');
-    titutlo.style.backgroundColor = '#FF00FF';}
-    else{console.log("acceso denegado")};
+    switch (tipoMoneda.value) {
+        case "pesos": dato =superficie_horizontalNeta_Pesos+""+superficie_horizontalTotal_Pesos +""+superficie_verticalNeta_Pesos + ""+ superficie_verticalTotal_pesos
+            break
 
+        case "dólar": dato = superficie_horizontalTotal_1usd+""+superficie_hotizontalNeta_1usd + ""+ superficie_verticalNeta_1usd + ""+ superficie_verticalTotal_1usd
+            break
 
-    let a= getElementById(imput1)
-    let b= getElementById(imput2)
-    const suma  = function (a, b) { return a + b };
-    const resta = function (a, b) { return a - b };
-    console.log(suma(a,b));
-    console.log(resta(a,b));
+        case "asignar":dato = info_deudaNeta + ""+ info_deudaTotal
+            break
+
+        default: dato = info_deudaNeta + ""+ deudaTotal
+    }
+    alert( opcionPresidencias + dato)
+};
+
+ 
