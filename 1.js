d9c816ceeb2f8294a2f829f002e047890059bbeb
jonathan <jonathan.mayan@bue.edu.ar>
2021-03-09 09:33:43 -0300

    falta refresh

--- 1.js
+++ 1.js
@@ -6,13 +6,13 @@
 // fuente Ministerio de finanzas,consultado 3/3/2021
 let opcionPresidencias= document.getElementById("opcionPresidencias").value;
 
-if (opcionPresidencias =="Mauricio Macri"){ console.log("presidente Mauricio")
-    deudaNeta= Number (82400000000),deudaTotal=Number (323065000000)}
+if (opcionPresidencias =="Mauricio Macri"){ console.log("presidente Mauricio");
+    deudaNeta= Number (82400000000);deudaTotal=Number (323065000000)}
 else if (opcionPresidencias =="Cristina Fernandez de Kirchner"){
-    deudaNeta= Number(75554000000),deudaTotal= Number(240665000000),console.log("presidente Cristina")
-}else{ console.log('sin designación de presidencia')}
+    deudaNeta= Number(75554000000),deudaTotal= Number(240665000000),console.log("presidente Cristina")}
+    else{ console.log('sin designación de presidencia')}
 
-   
+let tipoMoneda= (document.getElementById("input_tipoMoneda"));  
 let valorPeso= Number(document.getElementById("valor_delPeso").value);
 let tipo_billetePesos= Number(document.getElementById("input_denominacionBillete").value);
 let enPesos= valorPeso / tipo_billetePesos ;
@@ -26,33 +26,35 @@ superficie_horizontalTotal_Pesos= deudaTotal * enPesos * 0.10075 + "m2";
 superficie_verticalNeta_Pesos= deudaNeta * enPesos * 0.002 + " metros de altura";
 superficie_verticalTotal_pesos= deudaTotal * enPesos * 0.002 + " metros de altura";
 
-superficie_hotizontalNeta_1usd= deudaNeta * 0.10338947064 +" m2";
+superficie_horizontalNeta_1usd= deudaNeta * 0.10338947064 +" m2";
 superficie_horizontalTotal_1usd= deudaTotal * 0.10338947064 + " m2";
 
 superficie_verticalNeta_1usd= deudaNeta * 0.002 + " metros de altura";
 superficie_verticalTotal_1usd= deudaTotal * 0.002 + " metros de altura";
 
-let tipoMoneda= (document.getElementById("input_tipoMoneda"));
+function presidente( nombre, deudaNeta,deudaTotal,superficie_horizontalNeta_Pesos, superficie_horizontalNeta_1usd,)
+{ this.nombre= nombre, this.deudaNeta=deudaNeta, this.deudaTotal= deudaTotal,this.superficie_horizontalNeta_Pesos=superficie_horizontalNeta_Pesos, this.superficie_horizontalNeta_1usd=superficie_horizontalNeta_1usd }
 
-let mensaje= document.getElementById("campo_Informe") 
+const Macri= new presidente( "Mauricio Macri",82400000000,323065000000,superficie_horizontalNeta_Pesos,superficie_horizontalNeta_1usd );
+const Cristina= new presidente("Cristina Fernandez de kirchner",75554000000,240665000000,superficie_horizontalNeta_Pesos,superficie_horizontalNeta_1usd)
+
+ArrayPresidentes=[ Macri,Cristina ];
+seleccion=[]
 
 
 
-function infoDatos() {
+function informar(){switch(opcionPresidencias){
+    case "Mauricio Macri": document.getElementById("informe").innerHTML=
+    "<p>"+ "deuda neta:usd$"+ Macri.deudaNeta+"deuda total de Argentina al finalizar su mandato :usd$"+Macri.deudaTotal+ "</p>"+
+    "<p>"+"superficie horizontal que ocuaría la deuda Neta en billetes pesos: "+Macri.superficie_horizontalNeta_Pesos+"</p>"+
+    "<p>"+"superficie horizontal que ocuparía la deuda Neta en billetes dolares: "+Macri.superficie_horizontalNeta_1usd+"</p>";break;
+
+    case "Cristina Fernandez de Kirchner": document.getElementById("informe").innerHTML=
+    "<p>"+ "deuda neta:usd$"+ Cristina.deudaNeta+"deuda total de Argentina al finalizar su mandato :usd$"+Cristina.deudaTotal+ "</p>"+
+    "<p>"+"superficie horizontal que ocuaría la deuda Neta en billetes pesos: "+Cristina.superficie_horizontalNeta_Pesos+"</p>"+
+    "<p>"+"superficie horizontal que ocuparía la deuda Neta en billetes dolares: "+Cristina.superficie_horizontalNeta_1usd+"</p>"; break;
     
-    switch (tipoMoneda.value) {
-        case "pesos": dato =superficie_horizontalNeta_Pesos+""+superficie_horizontalTotal_Pesos +""+superficie_verticalNeta_Pesos + ""+ superficie_verticalTotal_pesos
-            break
+    default:document.getElementById("informe").innerHTML="<p>"+ "sin información"+ "</p>" }}
 
-        case "dólar": dato = superficie_horizontalTotal_1usd+""+superficie_hotizontalNeta_1usd + ""+ superficie_verticalNeta_1usd + ""+ superficie_verticalTotal_1usd
-            break
-
-        case "asignar":dato = info_deudaNeta + ""+ info_deudaTotal
-            break
-
-        default: dato = info_deudaNeta + ""+ deudaTotal
-    }
-    alert( opcionPresidencias + dato)
-};
 
  
