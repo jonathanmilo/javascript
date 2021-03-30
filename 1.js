7e45f3a907d69cc9ee2b7088f8d55dc49e07610d
jonathan <jonathan.mayan@bue.edu.ar>
2021-03-30 20:51:13 -0300

    cambios 26/3

--- 1.js
+++ 1.js
@@ -6,107 +6,90 @@
 // fuente Ministerio de finanzas,consultado 3/3/2021
 //pfeizer 14.19  CNBG 40
 
-
-let opcionPresidencias= document.getElementById("opcionPresidencias").value;
-
+let opcionPresidencias = $("#opcionPresidencias").val();
 localStorage.setItem("presidente",opcionPresidencias);
-
-localStorage.setItem("tipoDeVacuna",document.getElementById("input_tipoVacuna").value),
+$("#opcionPresidencias").change(()=>{
+    opcionPresidencias = $("#opcionPresidencias").val();
+    localStorage.setItem("presidente",opcionPresidencias);
+   // localStorage.setItem("presidente", JSON.stringify(opcionPresidencias)); 
+})
+localStorage.setItem("tipoDeVacuna",($("#input_tipoVacuna").val())),
 tipoVacuna=localStorage.getItem("tipoDeVacuna");
 
-localStorage.setItem("ValorPeso",Number(document.getElementById("valor_delPeso").value)),
-valorPeso=localStorage.getItem("ValorPeso");
+switch( $("#input_tipoVacuna").val()){
+    case "asignar":$("#input_ValorVacuna").css("display","flex");$("#fooValorVacuna").text("valor de vacuna usd$");ValorVacuna= $("#input_ValorVacuna").val(); localStorage.setItem("tipoDeVacuna",tipoVacuna); localStorage.setItem("valorVacuna",ValorVacuna); break;
+    case "pfeizer": ValorVacuna=14.19;$("#input_ValorVacuna").css("display","none");$("#fooValorVacuna").text("valor de vacuna usd$"+""+ValorVacuna+"");localStorage.setItem("tipoDeVacuna",tipoVacuna);localStorage.setItem("valorVacuna",ValorVacuna); break;
+    case "cnbg": ValorVacuna=40;$("#input_ValorVacuna").css("display","none");$("#fooValorVacuna").text("valor de vacuna usd$"+""+ValorVacuna+"");localStorage.setItem("tipoDeVacuna",tipoVacuna);localStorage.setItem("valorVacuna",ValorVacuna);break;    }
 
-// if(document.getElementById("input_tipoVacuna").value=="asignar")
-//     ValorVacuna=document.getElementById("input_ValorVacuna").value
-//      else ValorVacuna=localStorage.getItem("valorVacuna").value
+$("#input_tipoVacuna").change(()=>{
+ 
+    for(i=0;i<2;i++)
+    {switch( $("#input_tipoVacuna").val()){
+        case "asignar":$("#input_ValorVacuna").css("display","flex");$("#fooValorVacuna").text("valor de vacuna usd$");ValorVacuna= $("#input_ValorVacuna").val(); localStorage.setItem("tipoDeVacuna",tipoVacuna); localStorage.setItem("valorVacuna",ValorVacuna); break;
+        case "pfeizer": ValorVacuna=14.19;$("#input_ValorVacuna").css("display","none");$("#fooValorVacuna").text("valor de vacuna usd$"+""+ValorVacuna+"");localStorage.setItem("tipoDeVacuna",tipoVacuna);localStorage.setItem("valorVacuna",ValorVacuna); break;
+        case "cnbg": ValorVacuna=40;$("#input_ValorVacuna").css("display","none");$("#fooValorVacuna").text("valor de vacuna usd$"+""+ValorVacuna+"");localStorage.setItem("tipoDeVacuna",tipoVacuna);localStorage.setItem("valorVacuna",ValorVacuna);break;    }
+    }})
+  //  localStorage.setItem("tipoDeVacuna",JSON.stringify(tipoVacuna));
+  
+    
+    localStorage.setItem("ValorPeso",($("#valor_delPeso").val()));
+    valorPeso=localStorage.getItem("ValorPeso");
+    $("#valor_delPeso").change(()=>{ 
+         valorPeso=$("#valor_delPeso").val();
+        localStorage.setItem("ValorPeso",valorPeso);
+  //  localStorage.setItem("ValorPeso",Json.stringify(valorPeso))
+         });
 
-// localStorage.setItem("valorVacuna",ValorVacuna);
-function change(){
-    if(document.getElementById("input_tipoVacuna").value=="asignar"){
-        document.getElementById("input_ValorVacuna").style.display="flex";document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"}
-         else if(document.getElementById("input_tipoVacuna").value=="pfeizer"){ ValorVacuna=14.19; document.getElementById("input_ValorVacuna").style.display="none";document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"+ValorVacuna}
-         else if(document.getElementById("input_tipoVacuna").value=="cnbg"){ ValorVacuna=40;document.getElementById("input_ValorVacuna").style.display="none";document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"+ValorVacuna}
-         else{document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"}
-        }
-
-document.getElementById("input_ValorVacuna").addEventListener('change', change())
-
-
+$("#input_ValorVacuna").change(
+    ()=>{
+    ValorVacuna= $("#input_ValorVacuna").val();
+    localStorage.setItem("valorVacuna",ValorVacuna);
+    }
+);
 const aplicadas= 3548656;
 let censo= 44939000;
 
 function DATOS(){
-    localStorage.setItem("presidente",document.getElementById("opcionPresidencias").value);
-
+    
     if(localStorage.getItem("presidente")=="Mauricio Macri"){
         console.log("presidente Mauricio");deudaNeta= Number (82400000000);deudaTotal=Number (323065000000)}
         else if (localStorage.getItem("presidente") =="Cristina Fernandez de Kirchner"){
             deudaNeta= Number(75554000000),deudaTotal= Number(240665000000),console.log("presidente Cristina")}
             else{ console.log('sin designación de presidencia')} ;
-
             localStorage.setItem("presidente",opcionPresidencias);
-
-            localStorage.setItem("tipoDeVacuna",document.getElementById("input_tipoVacuna").value),
-            tipoVacuna=localStorage.getItem("tipoDeVacuna");
-            
-            localStorage.setItem("ValorPeso",Number(document.getElementById("valor_delPeso").value)),
-            valorPeso=localStorage.getItem("ValorPeso");
-            
-            function change(){
-                if(document.getElementById("input_tipoVacuna").value=="asignar"){
-                    document.getElementById("input_ValorVacuna").style.display="flex";document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"}
-                     else if(document.getElementById("input_tipoVacuna").value=="pfeizer"){ ValorVacuna=14.19; document.getElementById("input_ValorVacuna").style.display="none";document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"+ValorVacuna}
-                     else if(document.getElementById("input_tipoVacuna").value=="cnbg"){ ValorVacuna=40;document.getElementById("input_ValorVacuna").style.display="none";document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"+ValorVacuna}
-                     else{document.getElementById("fooValorVacuna").innerText="valor de vacuna usd$"}
-                    }
-            
-            document.getElementById("input_ValorVacuna").addEventListener('change', change())
-            
-
-            localStorage.setItem("valorVacuna",ValorVacuna);
-        
-
-dosis_deuda_neta=Number(deudaNeta/ValorVacuna).toFixed(1);
-dosis_deuda_total=Number(deudaTotal/ValorVacuna).toFixed(1);
-
-deuda_neta_pesos=Number(deudaNeta/valorPeso).toFixed(2);
-deuda_total_pesos=Number(deudaTotal/valorPeso).toFixed(2);
-
-}
-
-function grafica(){document.getElementById("seccion-grafico").style.display="flex";
-    document.getElementById("aplicadas_actuales").style.height=""+aplicadas *0.00001+"px";
-    document.getElementById("personas").style.height=""+ censo*0.00001 + "px"; 
-
-    if(document.getElementById("valor_delPeso").value>0){ document.getElementById("a-km-pe").style.height+=""+ dosis_deuda_neta *0.000001 +"px"; document.getElementById("n-km-p").innerText=(Number (dosis_deuda_neta)).toLocaleString("en-US")+ "dosis";
-document.getElementById("a-km-usd").style.height=""+dosis_deuda_total *0.000001 + "px";document.getElementById("n-km-usd").innerText=(Number (dosis_deuda_total)).toLocaleString("en-US")+"dosis"};
-
-document.getElementById("d1").innerText="usd$"+(Number(deudaNeta)).toLocaleString("en-US") ;
-document.getElementById("d2").innerText="usd$"+ (Number(deudaTotal)).toLocaleString("en-US") ;
-document.getElementById("d3").innerText= (Number(dosis_deuda_neta)).toLocaleString("en-US")+"dosis" ;
-document.getElementById("d4").innerText= (Number (dosis_deuda_total)).toLocaleString("en-US")+"dosis";
-document.getElementById("d5").innerText= "$"+(Number (deuda_neta_pesos)).toLocaleString("en-US");
-document.getElementById("d6").innerText= "$"+(Number (deuda_total_pesos)).toLocaleString("en-US") ;
-document.getElementById("aplicadasNumero").innerText= (Number (aplicadas)).toLocaleString("en-US") ;
-document.getElementById("personas_N").innerText=(Number(censo)).toLocaleString("en-US") ;}
-
     
 
-function informar(){switch(document.getElementById("opcionPresidencias").value){
-    case "Mauricio Macri":document.getElementById("informe").style.display="flex";document.getElementById("cara-presidente").src="./img/macri-sonrisa.png";grafica(); break;
+    dosis_deuda_neta=Number(deudaNeta/ValorVacuna).toFixed(1);
+    dosis_deuda_total=Number(deudaTotal/ValorVacuna).toFixed(1);
 
-    case "Cristina Fernandez de Kirchner": document.getElementById("informe").style.display="flex";document.getElementById("cara-presidente").src="./img/cristina-sonrisa.png";grafica();break;
-    default:document.getElementById("informe").innerHTML="<p>"+ "sin información"+ "</p>" } 
-    
+    deuda_neta_pesos=Number(deudaNeta/valorPeso).toFixed(2);
+    deuda_total_pesos=Number(deudaTotal/valorPeso).toFixed(2);
 }
 
-/*
-function presidente( nombre, deudaNeta,deudaTotal,superficie_horizontalNeta_1usd, superficie_horizontalTotal_1usd,superficie_verticalNeta_1usd,superficie_verticalTotal_1usd, superficie_verticalTotal_1usd,superficie_horizontalNeta_Pesos,superficie_horizontalTotal_Pesos,superficie_verticalNeta_Pesos,superficie_verticalTotal_pesos,moneda,valorPeso,TipoBillete)
-{ this.nombre= nombre, this.deudaNeta=deudaNeta, this.deudaTotal= deudaTotal,this.superficie_horizontalNeta_1usd=superficie_horizontalNeta_1usd,this.superficie_horizontalTotal_1usd=superficie_horizontalTotal_1usd,this.superficie_verticalNeta_1usd=superficie_verticalNeta_1usd,this.superficie_verticalTotal_1usd=superficie_verticalTotal_1usd,this.superficie_verticalNeta_Pesos=superficie_verticalNeta_Pesos,this.superficie_verticalTotal_pesos=superficie_verticalTotal_pesos,this.moneda=localStorage.getItem("tipoDeMoneda"),this.valorPeso=localStorage.getItem("ValorPeso"),this.TipoBillete=localStorage.getItem("TipoBillete")}
+function grafica(){$("#seccion-grafico").css("display","flex");
+   $("#aplicadas_actuales").css("height",""+aplicadas* 0.00001+"px"+"");
+   $("#personas").css("height",""+censo *0.00001+"px"+""); 
+  
+    if($("#valor_delPeso").val()>0){ $("#a-km-pe").css("height",""+dosis_deuda_neta * 0.000001+ "px"+"");$("#n-km-p").text(""+Number((dosis_deuda_neta)).toLocaleString("en-US")+ "dosis"+""); 
+    $("#a-km-usd").css("height",""+dosis_deuda_total * 0.000001+"px"+"");$("#n-km-usd").text(""+(Number(dosis_deuda_total)).toLocaleString("en-US")+"dosis"+"")};
 
-const Macri= new presidente( "Mauricio Macri",82400000000,323065000000,superficie_horizontalNeta_1usd,superficie_horizontalTotal_1usd,superficie_verticalNeta_1usd,superficie_verticalTotal_1usd,superficie_horizontalNeta_Pesos,superficie_horizontalTotal_Pesos,superficie_verticalNeta_Pesos,superficie_verticalTotal_pesos,tipoMoneda,valorPeso,TipoBillete);
-const Cristina= new presidente("Cristina Fernandez de kirchner",75554000000,240665000000,superficie_horizontalNeta_1usd,superficie_horizontalTotal_1usd,superficie_verticalNeta_1usd,superficie_verticalTotal_1usd,superficie_horizontalNeta_Pesos,superficie_horizontalTotal_Pesos,superficie_verticalNeta_Pesos,superficie_verticalTotal_pesos,tipoMoneda,valorPeso,TipoBillete)
 
-info[Cristina,Macri]
-   */
+    $("#d1").text(""+"usd"+(Number(deudaNeta)).toLocaleString("en-US")+"");
+    $("#d2").text(""+"usd"+(Number(deudaTotal)).toLocaleString("en-US")+"");
+    $("#d3").text(""+(Number(dosis_deuda_neta)).toLocaleString("en-US")+"dosis"+"");
+    $("#d4").text(""+(Number(dosis_deuda_total)).toLocaleString("en-US")+"dosis"+"");
+    $("#d5").text(""+"$"+(Number(deuda_neta_pesos)).toLocaleString("en-US")+"");
+    $("#d6").text(""+"$"+(Number(deuda_total_pesos)).toLocaleString("en-US")+"");
+    $("#aplicadasNumero").text(""+(Number(aplicadas)).toLocaleString("en-US")+"");
+    $("#personas_N").text(""+(Number(censo)).toLocaleString("en-US")+"");}
+
+
+
+
+function informar(){switch($("#opcionPresidencias").val()){
+    case "Mauricio Macri":$("#informe").css("display","flex");$("#cara-presidente").attr("src","./img/macri-sonrisa.png");grafica(); break;
+
+    case "Cristina Fernandez de Kirchner": document.getElementById("informe").style.display="flex";$("#cara-presidente").attr("src","./img/cristina-sonrisa.png");grafica();break;
+    default:$("#informe").html("<p>"+ "sin información"+ "</p>")} 
+   // document.getElementById("informe").innerHTML="<p>"+ "sin información"+ "</p>"
+}
